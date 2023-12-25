8305e38b6b7a765c4c1dbcee2bbe5f1c08bd6ecd
Saatvik Dhanta <139674788+S-Dhanta@users.noreply.github.com>
2023-12-25 12:36:03 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (6.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==1)
-            return 1;
-        else if(n==2)
-            return 2;
-        else if(n==3)
-            return 3;
-        else{
-            int i=4, last1=2, last2=3;
-            while(i<=n){
-                int temp=last2;
-                last2=last1+last2;
-                last1=temp;
-                i++;
-            }
-            return last2;
-        }
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        return climbStairs(n,dp,2);
+    }
+    private int climbStairs(int n,int[] dp,int i){
+        if(n<=3)
+            return n;
+        
+        if(i>n)
+            return n;
+        
+        dp[i]=dp[i-1]+dp[i-2];
+        climbStairs(n,dp,i+1);
+        
+        return dp[n];
     }
 }
